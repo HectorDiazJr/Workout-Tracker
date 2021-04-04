3cfddf16a1f579244eb003c5f04b2c217a308056
Hector Diaz <hectordiaz1103@gmail.com>
2021-04-04 10:29:04 -0400

    adding a console log to find the issue with heroku

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.static("public"));
 
 require("./routes/html-routes")(app);
 require("./routes/api-routes")(app);
+console.log("hope this works");
 console.log("this is mongodb uri");
 console.log(process.env.MONGODB_URI)
 
